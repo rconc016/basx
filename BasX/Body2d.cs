ba8ae4123add60a9b26f80b57c9b13fe54ae9028
Ronald Concepcion <rconc016@fiu.edu>
2019-01-29 18:21:34 -0500

    Added physical classes

---
+++ BasX/Body2d.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasX
+{
+    public class Body2d
+    {
+        public Vector2d Position { get; set; }
+        public Vector2d Velocity { get; set;  }
+        public Vector2d Acceleration { get; set; }
+        public double Mass { get; set; }
+
+        public Body2d()
+        {
+            Position = new Vector2d();
+            Velocity = new Vector2d();
+            Acceleration = new Vector2d();
+            Mass = 1.0;
+        }
+
+        /// <summary>
+        /// Updates the body's physical properties.
+        /// </summary>
+        /// <param name="deltaTime">The amount time in seconds since the last update.</param>
+        public void Update(double deltaTime)
+        {
+            Vector2d scaledAcceleration = new Vector2d(Acceleration.X * deltaTime, Acceleration.Y * deltaTime);
+
+            Velocity.Add(scaledAcceleration);
+
+            Position.Add(Velocity);
+        }
+
+        /// <summary>
+        /// Applies a force vector to this body.
+        /// </summary>
+        /// <param name="force">The force vector to apply.</param>
+        public void ApplyForce(Vector2d force)
+        {
+            Vector2d scaledForce = new Vector2d(force.X, force.Y);
+            scaledForce.Scale(1.0 / Mass);
+
+            Acceleration.Add(scaledForce);
+        }
+    }
+}
---
+++ BasX/World2d.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasX
+{
+    public class World2d
+    {
+        /// <summary>
+        /// The world's gravitational force.
+        /// </summary>
+        public double Gravity { get; set; }
+
+        /// <summary>
+        /// The list of bodies affected by the world.
+        /// </summary>
+        public IList<Body2d> Bodies { get; set; }
+
+        private long oldTime;
+
+        public World2d(double gravity = 0)
+        {
+            Gravity = gravity;
+            Bodies = new List<Body2d>();
+            oldTime = -1;
+        }
+
+        public void Update()
+        {
+            double deltaTime = UpdateTime();
+
+            Vector2d gravityVector = new Vector2d(0.0, Gravity * deltaTime);
+
+            foreach (Body2d body in Bodies)
+            {
+                body.ApplyForce(gravityVector);
+                body.Update(deltaTime);
+            }
+        }
+
+        /// <summary>
+        /// Updates the previously stored time.
+        /// </summary>
+        /// <returns>The delta time of the previously stored time.</returns>
+        private double UpdateTime()
+        {
+            if (oldTime == -1)
+            {
+                oldTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            }
+
+            double deltaTime = GetDeltaTime(oldTime);
+            oldTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+            return deltaTime;
+        }
+
+        /// <summary>
+        /// Calculates the current delta time.
+        /// </summary>
+        /// <param name="oldTime">Initial time in milliseconds.</param>
+        /// <returns>The difference between the old time and the current time in seconds.</returns>
+        private double GetDeltaTime(long oldTime)
+        {
+            return (DateTimeOffset.Now.ToUnixTimeMilliseconds() - oldTime) / 1000.0;
+        }
+    }
+}
