11ec5071c273a99f82aa295852a0b952bcb34f84
Ronald Concepcion <rconc016@fiu.edu>
2019-01-29 18:21:13 -0500

    Added vectors

---
+++ BasX/Vector.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BasX
+{
+    public class Vector
+    {
+        protected double[] Components { get; private set; }
+
+        protected Vector(int dimensions)
+        {
+            Components = new double[dimensions];
+        }
+
+        /// <summary>
+        /// Adds another vector to this one.
+        /// </summary>
+        /// <param name="other">The vector to be added.</param>
+        /// <returns>The resultant vector.</returns>
+        protected Vector Add(Vector other)
+        {
+            int minComponents = Math.Min(Components.Length, other.Components.Length);
+
+            for (int i = 0; i < minComponents; i++)
+            {
+                Components[i] += other.Components[i];
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Multiplies a scalar to this vector.
+        /// </summary>
+        /// <param name="scalar">The scalar to multiply by.</param>
+        /// <returns>The resultant vector.</returns>
+        protected Vector Scale(double scalar)
+        {
+            for (int i = 0; i < Components.Length; i++)
+            {
+                Components[i] *= scalar;
+            }
+
+            return this;
+        }
+    }
+}
---
+++ BasX/Vector2d.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasX
+{
+    public class Vector2d : Vector
+    {
+        public double X
+        {
+            get { return Components[0]; }
+            set { Components[0] = value; }
+        }
+
+        public double Y
+        {
+            get { return Components[1]; }
+            set { Components[1] = value; }
+        }
+
+        public Vector2d() : base(2)
+        {
+        }
+
+        public Vector2d(double x, double y) : this()
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Vector2d Add(Vector2d other)
+        {
+            return (Vector2d)base.Add(other);
+        }
+
+        public new Vector2d Scale(double scalar)
+        {
+            return (Vector2d) base.Scale(scalar);
+        }
+    }
+}
